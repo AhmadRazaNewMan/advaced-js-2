43b88891bf1e7ca1a29111394c075383bbf88882
AhmadRazaNewMan <webdevolopmentbyar@gmail.com>
2024-08-04 15:18:25 +0500

    adding new challenge

--- Problem-1.js
+++ Problem-1.js
@@ -1,1 +1,20 @@
-console.log("Hello world")
+//Count the occurrence of keys and convert the result into array of objects where each object belongs to one key and it's occurrence (count).
+let data =[
+    {language:"Javascript"},
+    {language:"Javascript"},
+    {language:"Javascript"},
+    {language:"c++"},
+    {language:"java"},
+    {language:"java"}
+
+]
+
+let result = data.reduce((acc,curr)=>{
+    if(acc[curr.language]){
+        acc[curr.language] = acc[curr.language]+1
+    }else{
+        acc[curr.language] = 1
+    }
+    return acc
+},{})
+console.log(result)
