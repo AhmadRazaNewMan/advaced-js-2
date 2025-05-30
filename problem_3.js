cbbe5ca2507dcb3541c2295dd7dfa06d41d8cb9f
22pwbcs0901@uetpeshawar.edu.pk <webdevolopmentbyar@gmail.com>
2025-05-30 13:49:38 +0500

    object converstion to array

--- problem_3.js
+++ problem_3.js
@@ -1,6 +1,9 @@
 const dummyObject = {a: 1, b: 2, c: 3};
 
 const arr = Object.entries(dummyObject)
-
-console.dir(arr, {depth: null, colors: true});
-console.log(Array.isArray(arr)); // true
+const values = arr.map((item)=>{
+    return item[1];// [1,2,3] if you want to get values
+    // return item[0]; // ['a', 'b', 'c'] if you want to get keys
+    // return item; // [['a', 1], ['b', 2], ['c', 3]] if you want to get both keys and values
+})
+console.log(values)
